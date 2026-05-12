95d5b607be1940214aeaf2965c1ca49c0b0fb490
Tjitouua <veriimapoha@gmail.com>
2026-05-12 10:35:33 +0200

    Still busy with the login page

--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -9,21 +9,21 @@ const LoginForm = () => {
             <div className="w-full bg-primaryy flex pt-4 justify-center"><div className="w-50 ml-15 h-28 mb-5 bg-[url('images/ncaa-logo.png')] bg-center bg-cover"></div></div>
             {/* Form  */}
             <div className="flex flex-col gap-1 text-gray-700 px-5 xl:px-8 justify-center pb-10 py-5">
-            <label className="font-extrabold font-serif text-3xl">Sign In</label>
+            <label className="font-extrabold font-serif text-2xl">Sign In</label>
             <label className="mb-4 text-xs">Use your NCAA credentials to access the system.</label>
             {/* Username  */}
             <div className="flex flex-col gap-1 mb-2">
-                <label className="font-bold">Username</label>
+                <label className="font-bold text-sm">Username</label>
                 <input className="w-full py-2 border rounded-lg px-3 border-black/50 placeholder:text-sm outline-none " type="text" placeholder="e.g. hangula.e" />
             </div>
             {/* Password  */}
             <div className="flex flex-col gap-1 mb-2">
-                <label className="font-bold">Password</label>
+                <label className="font-bold text-sm">Password</label>
                 <input className="w-full py-2 border rounded-lg px-3 border-black/50 outline-none" type="password" placeholder="••••••••" />
             </div>
             {/* Sign in as  */}
             <div className="flex flex-col gap-1 mb-5">
-                <label className="font-bold">Sign in as</label>
+                <label className="font-bold text-sm">Sign in as</label>
                 <select className="w-full cursor-pointer text-sm text-gray-500 border-black/50 py-2 border rounded-lg px-3 pr-20 outline-none">
                     <option>Employee</option>
                     <option>Training officer</option>
