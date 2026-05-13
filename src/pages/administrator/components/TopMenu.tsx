1feefeaf4e43b560e1c7096ece8ef965fe4a02d3
Tjitouua <veriimapoha@gmail.com>
2026-05-13 17:08:17 +0200

    Still busy with the top menu

--- src/pages/administrator/components/TopMenu.tsx
+++ src/pages/administrator/components/TopMenu.tsx
@@ -1,10 +1,41 @@
+import { useEffect, useRef, useState } from "react";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { MdOutlineLogout } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 
 const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
+
+    const navigate = useNavigate();
+
+
+    const [showPopup, setShowPop] = useState(false);
+
+    const popupRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if(
+                popupRef.current &&
+                !popupRef.current.contains(event.target)
+            ) {
+                setShowPop(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+           document.removeEventListener("mousedown", handleClickOutside);
+        };
+
+    }, []);
+
+
+
     return (
-        <div className="w-full py-3 bg-white border-l border-l-secondaryy flex px-2 md:px-6 justify-between items-center">
+        <div ref={popupRef} className="w-full py-3 bg-white border-l border-l-secondaryy flex px-2 md:px-6 justify-between items-center">
             {/* Name  */}
             <div className="flex items-center gap-5">
                 <MdOutlineSpaceDashboard 
@@ -25,7 +56,7 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
                 </div>
 
                 {/* User  */}
-                <div className="flex items-center gap-3 cursor-pointer hover:bg-secondary/20 px-1 py-[2px] pr-2 rounded-full">
+                <div onClick={() => setShowPop(!showPopup)} className="flex items-center gap-3 cursor-pointer hover:bg-secondary/20 px-1 py-[2px] pr-2 rounded-full">
                     <div className="bg-secondaryy p-2 rounded-full">
                        <label className="text-md font-bold">AD</label>
                     </div>
@@ -34,6 +65,25 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
                         <label className="-mt-[2px]">Admin</label>
                     </div>
                 </div>
+            </div>
+
+
+            {/* Pop Ups  */}
+            <div className="right-7 z-50 fixed w-50 mt-43 flex items-center justify-end">
+
+               {/* Logout  */}
+               {showPopup && (
+               <div className="flex flex-col w-300 bg-white shadow-sm shadow-black/30 rounded-lg">
+                  <div className="flex py-4 px-3 flex-col border-b border-secondaryy">
+                    <label className="font-bold text-sm">Administrator</label>
+                    <label className="text-xs">admin • Admin</label>
+                  </div>
+                  <div className="flex px-2 py-1 gap-3">
+                     <button onClick={() => navigate("/")} className="w-full py-2 px-2 justify-start flex gap-3 rounded-sm items-center hover:bg-secondaryy cursor-pointer hover:text-red-600"><MdOutlineLogout /> Sign out</button>
+                  </div>
+               </div>
+               )}
+
             </div>
 
         </div>
