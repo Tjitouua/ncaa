2cf3f85b784e024fedc3f03300f24fdf3018a84c
Tjitouua <veriimapoha@gmail.com>
2026-05-18 09:29:09 +0200

    Finished with the Notifications component in the top bar

--- src/pages/administrator/components/TopMenu.tsx
+++ src/pages/administrator/components/TopMenu.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { IoNotificationsOutline } from "react-icons/io5";
 import { MdOutlineSpaceDashboard } from "react-icons/md";
 import { MdOutlineLogout } from "react-icons/md";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
@@ -10,6 +10,7 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
     const navigate = useNavigate();
 
 
+    // Logout 
     const [showPopup, setShowPop] = useState(false);
 
     const popupRef = useRef(null);
@@ -21,6 +22,30 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
                 !popupRef.current.contains(event.target)
             ) {
                 setShowPop(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+
+        return () => {
+           document.removeEventListener("mousedown", handleClickOutside);
+        };
+
+    }, []);
+
+
+    // Notifications 
+    const [showPopup2, setShowPop2] = useState(false);
+
+    const popupRef2 = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if(
+                popupRef.current &&
+                !popupRef.current.contains(event.target)
+            ) {
+                setShowPop2(false);
             }
         };
 
@@ -46,7 +71,7 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
 
             <div className="flex items-center gap-3">
                 {/* Notifications  */}
-                <div className="flex gap-1 items-center px-1 rounded-lg cursor-pointer hover:bg-secondaryy">
+                <div onClick={() => setShowPop2(!showPopup2)} className="flex gap-1 items-center px-1 rounded-lg cursor-pointer hover:bg-secondaryy">
                    <IoNotificationsOutline className="font-bold text-xl" />
                    <div className="h-8">
                      <div className="text-white flex items-center -ml-1 justify-center w-4 p-1 h-4 rounded-full bg-red-600 hover:bg-secondary/30">
@@ -69,12 +94,34 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
 
 
             {/* Pop Ups  */}
-            <div className="right-7 z-50 fixed w-50 mt-43 flex items-center justify-end">
+            <div className="right-7 z-50 fixed top-15 flex items-start justify-end">
+
+               {/* Notifications  */}
+               {showPopup2 && (
+               <div className="flex flex-col min-w-75 bg-white shadow-sm shadow-black/30 rounded-lg">
+                   <div className="w-full py-3 px-3 flex items-center justify-between border-b border-secondaryy">
+                      <label className="font-bold">Notifications</label>
+                      <div className="">
+                        <label className="text-xs hover:underline cursor-pointer">View all</label>
+                      </div>
+                   </div>
+                   <div className="w-full py-2 px-3 flex flex-col items-start border-t border-secondaryy">
+                      <label className="font-bold">Certification expiring soon</label>
+                      <label className="text-xs hover:underline">AVSEC-2024-042 expires in 20 days.</label>
+                      <label className="text-[10px]">5/19/2026</label>
+                   </div>
+                   <div className="w-full py-2 px-3 flex flex-col items-start border-t border-secondaryy">
+                      <label className="font-bold">Certification expiring soon</label>
+                      <label className="text-xs hover:underline">AVSEC-2024-042 expires in 10 days.</label>
+                      <label className="text-[10px]">5/19/2026</label>
+                   </div>
+               </div>
+               )}
 
                {/* Logout  */}
                {showPopup && (
-               <div className="flex flex-col w-300 bg-white shadow-sm shadow-black/30 rounded-lg">
-                  <div className="flex py-4 px-3 flex-col border-b border-secondaryy">
+               <div className="flex flex-col min-w-50 bg-white shadow-sm shadow-black/30 rounded-lg">
+                  <div className="flex py-3 px-3 flex-col border-b border-secondaryy">
                     <label className="font-bold text-sm">Administrator</label>
                     <label className="text-xs">admin • Admin</label>
                   </div>
@@ -83,6 +130,7 @@ const TopMenu = ({ setShowMenu, title = "Dashboard" }) => {
                   </div>
                </div>
                )}
+
 
             </div>
 
