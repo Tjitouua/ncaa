ed4e73b513c758bb85f2bf7da9e136e3f7e3e41a
Tjitouua <veriimapoha@gmail.com>
2026-05-18 15:37:32 +0200

    Still busy with the Admin dashboard page

--- src/pages/administrator/components/DashboardPart.tsx
+++ src/pages/administrator/components/DashboardPart.tsx
@@ -58,7 +58,7 @@ const DashboardPart = () => {
      return (
         <div className="w-full min-h-screen text-secondary/90 px-2 md:px-6">
             {/* Welcome Div  */}
-            <div className="flex flex-col mt-8 mb-1">
+            <div className="flex flex-col mt-9 mb-1">
                <label className="font-bold text-xl">Welcome back, Administrator</label>
                <label className="text-secondary/60 text-sm">Overview of NCAA staff training & completion</label>
             </div>
