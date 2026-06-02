2ca0dc945834af37a0fb8decbb7441fcd7897a23
Tjitouua <veriimapoha@gmail.com>
2026-06-02 08:30:02 +0200

    Started with the staff side

--- src/pages/staff/Dashboard.tsx
+++ src/pages/staff/Dashboard.tsx
@@ -16,6 +16,13 @@ const Dashboard2 = () => {
           <Menu showMenu={showMenu} setShowMenu={setShowMenu} />
           <div className="w-full xl:w-[82%] min-h-screen text-secondary/80 bg-secondaryy">
              <TopMenu setShowMenu={setShowMenu} title="My Dashboard" />
+             <div className="w-full min-h-screen text-secondary/90 px-2 md:px-6">
+                 {/* Welcome Div  */}
+            <div className="flex flex-col mt-9 mb-1">
+               <label className="font-bold text-xl">Welcome, Tjitouua Mapoha</label>
+               <label className="text-secondary/50 text-sm">Software Developer · ICT</label>
+            </div>
+             </div>
           </div>
        </div>
     );
